ea1cd517356b33c59ae1b4a059ad49dfe955fe3a
Tomas Knapen <tknapen@gmail.com>
2023-03-16 08:52:23 +0100

    compile attempt 1

--- create.sh
+++ create.sh
@@ -6,7 +6,7 @@
 echo "VERSION NUMBER: " $1 "PRODUCING FILE versions/paper_${1}.pdf"
 echo "GIT COMMIT MESSAGE: " $2
 
-pandoc --verbose --filter pandoc-crossref --citeproc --filter packages/pandoc-wrapfig/pandoc-wrapfig.py --bibliography lib/export_2023-3-16.bib --pdf-engine=xelatex --csl=templates/nature.csl --template=templates/default.latex paper.md -f markdown -t latex -s -o versions/paper_${1}.pdf
+pandoc --verbose --citeproc --filter packages/pandoc-wrapfig/pandoc-wrapfig.py --bibliography lib/export_2023-3-16.bib --pdf-engine=xelatex --csl=templates/nature.csl --template=templates/default.latex paper.md -f markdown -t latex -s -o versions/paper_${1}.pdf
 git add versions/paper_${1}.pdf
 
 # pandoc --verbose --citeproc --bibliography lib/vici2020.bib --csl=templates/nature.csl preproposal.md -f markdown -s -o versions/vici_preproposal_${1}.docx
